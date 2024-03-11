77ff882a11944608ef66c090df97b3f99b6c0e05
charlie_lin <charlie_lin@gss.com.tw>
2024-03-11 14:06:57 +0800

    新增 -註解 -報表版本

--- auto_py/formatBTData/main_report.py
+++ auto_py/formatBTData/main_report.py
@@ -1,30 +1,29 @@
 import openpyxl
 import csv
 import os
+import datetime
 
 wb = openpyxl.Workbook()
 
-# 定義function，傳入csv路徑，轉檔至xlsx
 def transCsvDataToXlsx(fileName):
-    # 讀取並解析csv檔
     file = open(fileName)
     reader = csv.reader(file)
     data_list = list(reader)
     file.close()
 
-    # 抓股票代號，存成一個工作表
     stockId = data_list[1][1].replace('=','').replace('"','')
-    wb.create_sheet(stockId)
-    resultSheet = wb[stockId]
+    sheetName = stockId + '_BT' + datetime.date.today().strftime('%y%m%d')
+    wb.create_sheet(sheetName)
+    resultSheet = wb[sheetName]
 
-    # 抓取並寫入欄位名稱
-    resultSheet.cell(1,1,data_list[2][0])
-    resultSheet.cell(1,2,data_list[2][1])
-    resultSheet.cell(1,3,data_list[2][2])
-    resultSheet.cell(1,4,data_list[2][3])
-    resultSheet.cell(1,5,data_list[2][4])
+    # 寫入欄位名稱
+    resultSheet.cell(1,1,'ID')
+    resultSheet.cell(1,2,'BROKER')
+    resultSheet.cell(1,3,'PRICE')
+    resultSheet.cell(1,4,'BUY')
+    resultSheet.cell(1,5,'SELL')
 
-    n=1 # n記錄寫到第幾列
+    n=1
     for i in range(3,len(data_list)):
         n+=1
         resultSheet.cell(n,1,data_list[i][0]).data_type = 'int'
@@ -43,14 +42,12 @@ def transCsvDataToXlsx(fileName):
             resultSheet.cell(n,5,data_list[i][10].replace(',','')).data_type = 'int'
 
 #轉檔
-dir_path = os.path.dirname(os.path.realpath(__file__)) # 抓主程式目錄
-dir = os.listdir(dir_path) # 抓目錄下檔案
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dir = os.listdir(dir_path)
 
-# 如果是csv檔就呼叫function轉檔
 for file in dir:
     print(file)
     if file.split('.')[1].upper() == 'CSV':
         transCsvDataToXlsx(dir_path + '\\' + file)
 
-# 結果存至result.xlsx
 wb.save(dir_path + '\\' + 'result.xlsx')
