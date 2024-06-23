01b657762dfcacfe9259f6d12b78881b77168a8a
charlie_lin <charlie_lin@gss.com.tw>
2024-06-24 00:09:48 +0800

    總量排行表查詢介面and資料

---
+++ chart1/script/main.js
@@ -0,0 +1,55 @@
+$(function(){
+  $.ajaxSetup({ cache: false });
+  var stock = $('#stock').attr('value');
+  var type = $('#tableType').attr('value');
+  GetDataAndShowContent(stock+type+'.csv', '');  
+});
+
+function confirm(){
+    var stock = $('#stock').attr('value');
+    var queryDate = $('#QueryDate').val();
+    var type = $('#tableType').attr('value');
+    GetDataAndShowContent(stock+type+'.csv', queryDate);
+}
+
+function GetDataAndShowContent(fileName, date){
+  var csvData = [];
+  $.ajax({
+    type: "GET",
+    url: "/chart1/data/"+fileName,
+    dataType: "text",
+    success: function(csv) {
+      csvData = $.csv.toObjects(csv);
+      if(date == ''){
+        $('#content').html(csvData[csvData.length-1].content);
+      }
+      else{
+        var noData = true;
+        for (let i = 0; i < csvData.length; ++i) {
+          if(date == csvData[i].Date){
+            $('#content').html(csvData[i].content);
+            noData = false;
+            break;
+          }
+        }
+
+        if(noData){
+          $('#content').html('查無資料')
+        }
+      }
+    },
+    error: function(r){
+      alert('error');
+    }
+  });
+}
+
+function changeStock(id, name){
+  $('#stock').attr('value', id);
+  $('#stock').html(name);
+}
+
+function changeType(type, text){
+  $('#tableType').attr('value', type);
+  $('#tableType').html(text);
+}
