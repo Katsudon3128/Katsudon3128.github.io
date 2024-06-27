6fc9c794096473a417a8ac8f8714d8c509666477
charlie_lin <charlie_lin@gss.com.tw>
2024-06-27 23:25:29 +0800

    chart1 新增[前一天/後一天]功能

--- chart1/script/main.js
+++ chart1/script/main.js
@@ -12,6 +12,33 @@ function confirm(){
     GetDataAndShowContent(stock+type+'.csv', queryDate);
 }
 
+function addDays(days){
+  var stock = $('#stock').attr('value');
+  var type = $('#tableType').attr('value');
+  var fileName = stock+type+'.csv';
+  var id = parseInt($('#rowId').val());
+  id += days;
+  
+  var csvData = [];
+  $.ajax({
+    type: "GET",
+    url: "/chart1/data/"+fileName,
+    dataType: "text",
+    success: function(csv) {
+      csvData = $.csv.toObjects(csv);
+      if(csvData[id] != undefined){
+        $('#content').html(csvData[id].content);
+        $('#rowId').val(id);
+      }else{
+        alert('查無資料')
+      }
+    },
+    error: function(r){
+      alert('error');
+    }
+  });
+}
+
 function GetDataAndShowContent(fileName, date){
   var csvData = [];
   $.ajax({
@@ -22,12 +49,14 @@ function GetDataAndShowContent(fileName, date){
       csvData = $.csv.toObjects(csv);
       if(date == ''){
         $('#content').html(csvData[csvData.length-1].content);
+        $('#rowId').val(csvData.length-1);
       }
       else{
         var noData = true;
         for (let i = 0; i < csvData.length; ++i) {
           if(date == csvData[i].Date){
             $('#content').html(csvData[i].content);
+            $('#rowId').val(i);
             noData = false;
             break;
           }
