014fa1e31855f47ad99908d3387bfce45ee68241
CharlieLin <holeinone111111@gmail.com>
2024-07-20 23:54:08 +0800

    update base

--- plog/astro.config.mjs
+++ plog/astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 // https://astro.build/config
 export default defineConfig({
   site: 'https://katsudon3128.github.io',
-  base: '/plog',
+  base: '/',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
